70ce15bc6e3e9efddaaf69e56593a3bb7f91172c
Emyjakarta <ukonud4@gmail.com>
2024-01-18 12:57:27 +0100

    function that sorts an array of integers in ascending order using the Bubble sort algorithm

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * _putchar-prints a single character
+ * @c: character to be printed
+ * Return: c (character)
+ */
+char _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
