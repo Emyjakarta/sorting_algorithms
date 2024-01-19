f5f8a611f3a5475deb3a06570bbab357ddaa1f76
Emyjakarta <ukonud4@gmail.com>
2024-01-19 09:19:53 +0100

    correction to task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,35 @@
 #include "sort.h"
 /**
+ * switch_nodes-swaps two nodes
+ * @list: doubly linked list
+ * @node1: first node to be swapped
+ * @node2: second node to be swapped
+ */
+void switch_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	listint_t *prev1 = NULL, *next2 = NULL;
+
+	if (node1 == NULL || node2 == NULL)
+	{
+		return;
+	}
+	prev1 = node1->prev;
+	next2 = node2->next;
+
+	if (prev1 != NULL)/*checks if node 1 is the head node*/
+		prev1->next = node2;
+	if (next2 != NULL)/*checks if node 2 is the head node*/
+		next2->prev = node1;
+
+	node1->next = next2;
+	node1->prev = node2;
+	node2->next = node1;
+	node2->prev = prev1;
+
+	if (prev1 == NULL)
+		*list = node2;
+}
+/**
  * insertion_sort_list-function that sorts
  * a doubly linked list of integers
  * in ascending order using the insertion sort algorithm
@@ -8,42 +38,22 @@
 void insertion_sort_list(listint_t **list)
 {
 
-	listint_t *sorted = NULL;
-	listint_t *new = *list;
-	listint_t *next = NULL;
-	listint_t *temp = NULL;
+	/*listint_t *sorted = NULL;*//*sorted list*/
+	listint_t *present = *list;/*present node being inserted*/ 
+	listint_t *prev = NULL;/*previous node to be inserted*/
+	/*listint_t *temp = NULL;*//*temporary node*/
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	while (new != NULL)
+	while (present != NULL)
 	{
-		next = new->next;
-		if (sorted == NULL || sorted->n >= new->n)
+		prev = present->prev;
+		while (prev && prev->n > present->n)
 		{
-			new->next = sorted;
-			new->prev = NULL;
-			if (sorted != NULL)
-			{
-				sorted->prev = new;
-			}
-			sorted = new;
+			switch_nodes(list, prev, present);
 			print_list(*list);
+			prev = present->prev;
 		}
-		else
-		{
-			temp = sorted;
-			while (temp->next != NULL && temp->next->n < new->n)
-			{
-				temp = temp->next;
-			}
-			new->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = new;
-			temp->next = new;
-			new->prev = temp;
-			print_list(*list);
-		}
-		new = next;
+		present = present->next;
 	}
-	*list = sorted;
 }
